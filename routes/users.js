4b172588bf226aa978950adf422f263f060e8156
Christopher Metz <cmetz7@gmail.com>
2017-01-09 10:38:35 -0800

    Cleaned up part 3

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const knex = require('../knex');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const { camelizeKeys } = require('humps');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 router.post('/users', (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
@@ -16,7 +16,7 @@ router.post('/users', (req, res, next) => {
     return next(boom.create(400, 'Email must not be blank'));
   }
 
-  if (!password || !password.trim()) {
+  if (!password || password.length < 8) {
     return next(boom.create(400, 'Password must be at least 8 characters long')); // eslint-disable-line max-len
   }
 
@@ -28,28 +28,19 @@ router.post('/users', (req, res, next) => {
         return next(boom.create(400, 'Email already exists'));
       }
 
-      bcrypt.hash(req.body.password, 12)
+      return bcrypt.hash(password, 12);
+    })
 
-      // eslint-disable-next-line camelcase
-        .then((hashed_password) => {
-          return knex('users')
-            .insert({
-              first_name: firstName, // eslint-disable-line camelcase
-              last_name: lastName, // eslint-disable-line camelcase
-              email: email, // eslint-disable-line object-shorthand
-              hashed_password: hashed_password // eslint-disable-line camelcase, object-shorthand, max-len
-            }, '*');
-        })
-        .then((users) => {
-          const user = users[0];
+    .then((hashedPassword) => {
+      return knex('users')
+        .insert(decamelizeKeys({ firstName, lastName, email, hashedPassword }), '*'); // eslint-disable-line max-len
+    })
+    .then((users) => {
+      const user = users[0];
 
-          delete user.hashed_password;
+      delete user.hashed_password;
 
-          res.send(camelizeKeys(user));
-        })
-        .catch((err) => {
-          next(err);
-        });
+      res.send(camelizeKeys(user));
     })
     .catch((err) => {
       next(err);
